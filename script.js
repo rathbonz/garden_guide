b2cb2de7978ea875783e37aeaa5b1efd5ba0e635
rathbonz <59523748+rathbonz@users.noreply.github.com>
2022-08-23 22:11:40 -0700

    Main

---
+++ script.js
@@ -0,0 +1,9 @@
+function showFormNew() {
+    document.getElementById("new-user-id").style.display = "block";
+    document.getElementById("existing-user-id").style.display = "none";
+}
+
+function showFormExisting() {
+    document.getElementById("existing-user-id").style.display = "block";
+    document.getElementById("new-user-id").style.display = "none";
+}
